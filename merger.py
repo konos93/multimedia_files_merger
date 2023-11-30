2b6b76ea9e8559e744b931d48bf693b89faa01f4
konos93 <konos_mk93@hotmail.com>
2023-11-30 12:11:42 +0200

    Update merger.py

--- merger.py
+++ merger.py
@@ -14,36 +14,44 @@ file_paths = filedialog.askopenfilenames(title="Select files", filetypes=[("All 
 if not file_paths:
     print("No files selected. Exiting.")
 else:
-    # Save absolute file paths to selected_files.txt
-    with open('selected_files.txt', 'w') as file:
-        for file_path in file_paths:
-            file.write(f'file \'{os.path.abspath(file_path)}\'\n')
-
-    print('Selected file paths have been saved to selected_files.txt')
-
-    # Define the FFmpeg command as a list of arguments
-    ffmpeg_command = [
-        'ffmpeg',
-        '-f', 'concat',
-        '-safe', '0',
-        '-i', 'selected_files.txt',
-        '-c', 'copy',
-        'output.mp3'
-    ]
-
-    # Check if the file exists before running the FFmpeg command
-    if not os.path.exists('selected_files.txt'):
-        print("Error: selected_files.txt does not exist.")
+    # Check if all selected files have the same extension
+    extensions = {os.path.splitext(file_path)[1] for file_path in file_paths}
+    if len(extensions) > 1:
+        print("Warning: Selected files have different extensions. Please choose files with the same extension for concatenation.")
     else:
-        # Execute the FFmpeg command
-        subprocess.run(ffmpeg_command)
+        # Save absolute file paths to selected_files.txt
+        with open('selected_files.txt', 'w' , encoding='utf-8') as file:
+            for file_path in file_paths:
+                file.write(f'file \'{os.path.abspath(file_path)}\'\n')
 
- 
- # Delete the selected_files.txt file
-    try:
-        os.remove('selected_files.txt')
-       # print("selected_files.txt deleted.")
-    except OSError as e:
-        print(f"Error deleting selected_files.txt: {e}")
+        print('Selected file paths have been saved to selected_files.txt')
 
-print("Concatenation completed.")
+        # Extract the file extension from the first file in the list
+        _, file_extension = os.path.splitext(file_paths[0])
+
+        # Define the FFmpeg command as a list of arguments
+        output_file = f'output{file_extension}'
+        ffmpeg_command = [
+            'ffmpeg',
+            '-f', 'concat',
+            '-safe', '0',
+            '-i', 'selected_files.txt',
+            '-c', 'copy',
+            output_file
+        ]
+
+# Check if the file exists before running the FFmpeg command
+if not os.path.exists('selected_files.txt'):
+    print("Error: selected_files.txt does not exist.")
+else:
+    # Execute the FFmpeg command
+    subprocess.run(ffmpeg_command)
+
+# Delete the selected_files.txt file
+try:
+    os.remove('selected_files.txt')
+   # print("selected_files.txt deleted.")
+except OSError as e:
+    print(f"Error deleting selected_files.txt: {e}")
+
+print("Concatenation completed. Output file:", output_file)
