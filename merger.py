29e51687872f4b8ff0ee50dcd0a93c910faafd4a
konos93 <konos_mk93@hotmail.com>
2023-12-25 17:14:50 +0200

    Update merger.py

--- merger.py
+++ merger.py
@@ -2,56 +2,62 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
-# Create a GUI window for file selection
-root = tk.Tk()
-root.withdraw()  # Hide the main window
+def concatenate_files(file_paths, output_file):
+    # Save absolute file paths to selected_files.txt
+    with open('selected_files.txt', 'w', encoding='utf-8') as file:
+        for file_path in file_paths:
+            file.write(f'file \'{os.path.abspath(file_path)}\'\n')
 
-# Prompt the user to select one or more files
-file_paths = filedialog.askopenfilenames(title="Select files", filetypes=[("All Files", "*.*")])
+    print('Selected file paths have been saved to selected_files.txt')
 
-# Check if the user selected any files or canceled
-if not file_paths:
-    print("No files selected. Exiting.")
-else:
-    # Check if all selected files have the same extension
-    extensions = {os.path.splitext(file_path)[1] for file_path in file_paths}
-    if len(extensions) > 1:
-        print("Warning: Selected files have different extensions. Please choose files with the same extension for concatenation.")
+    # Extract the file extension from the first file in the list
+    _, file_extension = os.path.splitext(file_paths[0])
+
+    # Define the FFmpeg command as a list of arguments
+    ffmpeg_command = [
+        'ffmpeg',
+        '-f', 'concat',
+        '-safe', '0',
+        '-i', 'selected_files.txt',
+        '-c', 'copy',
+        output_file
+    ]
+
+    # Check if the file exists before running the FFmpeg command
+    if not os.path.exists('selected_files.txt'):
+        print("Error: selected_files.txt does not exist.")
     else:
-        # Save absolute file paths to selected_files.txt
-        with open('selected_files.txt', 'w' , encoding='utf-8') as file:
-            for file_path in file_paths:
-                file.write(f'file \'{os.path.abspath(file_path)}\'\n')
+        # Execute the FFmpeg command
+        subprocess.run(ffmpeg_command)
+'''
+    # Delete the selected_files.txt file
+    try:
+        os.remove('selected_files.txt')
+    except OSError as e:
+        print(f"Error deleting selected_files.txt: {e}")
 
-        print('Selected file paths have been saved to selected_files.txt')
+    print("Concatenation completed. Output file:", output_file)
+'''
+def main():
+    # Create a GUI window for file selection
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
 
-        # Extract the file extension from the first file in the list
-        _, file_extension = os.path.splitext(file_paths[0])
+    # Prompt the user to select one or more files
+    file_paths = filedialog.askopenfilenames(title="Select files", filetypes=[("All Files", "*.*")])
 
-        # Define the FFmpeg command as a list of arguments
-        output_file = f'output{file_extension}'
-        ffmpeg_command = [
-            'ffmpeg',
-            '-f', 'concat',
-            '-safe', '0',
-            '-i', 'selected_files.txt',
-            '-c', 'copy',
-            output_file
-        ]
+    # Check if the user selected any files or canceled
+    if not file_paths:
+        print("No files selected. Exiting.")
+    else:
+        # Specify the output file name
+        output_file = f'output{os.path.splitext(file_paths[0])[1]}'
 
-# Check if the file exists before running the FFmpeg command
-if not os.path.exists('selected_files.txt'):
-    print("Error: selected_files.txt does not exist.")
-else:
-    # Execute the FFmpeg command
-    subprocess.run(ffmpeg_command)
+        # Use ThreadPoolExecutor for concurrent processing
+        with ThreadPoolExecutor() as executor:
+            executor.submit(concatenate_files, file_paths, output_file)
 
-# Delete the selected_files.txt file
-try:
-    os.remove('selected_files.txt')
-   # print("selected_files.txt deleted.")
-except OSError as e:
-    print(f"Error deleting selected_files.txt: {e}")
-
-print("Concatenation completed. Output file:", output_file)
+if __name__ == "__main__":
+    main()
